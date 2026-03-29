9d9e8f77e8bc8a9498ef19b667614823b3d8a37f
anayi89 <it.garry@gmail.com>
2026-03-28 21:05:03 -0400

    Add Notion API handler function

---
+++ netlify/functions/notion.js
@@ -0,0 +1,27 @@
+const NOTION_SECRET = process.env.NOTION_SECRET;
+const NOTION_VERSION = "2022-06-28";
+
+exports.handler = async (event) => {
+  const path = event.path.replace("/.netlify/functions/notion", "");
+  const url = `https://api.notion.com/v1${path}`;
+
+  const headers = {
+    "Authorization": `Bearer ${NOTION_SECRET}`,
+    "Notion-Version": NOTION_VERSION,
+    "Content-Type": "application/json",
+  };
+
+  const response = await fetch(url, {
+    method: event.httpMethod,
+    headers,
+    body: event.body || undefined,
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: response.status,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(data),
+  };
+};
